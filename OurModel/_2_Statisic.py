a097e4c6ba6d789eeea23c29ee44fa26458c16c3
stormbroken <181250184@smail.nju.edu.cn>
2020-07-15 18:47:05 +0800

    feat: 更新了有效提交和有效提交率

--- OurModel/_2_Statisic.py
+++ OurModel/_2_Statisic.py
@@ -231,14 +231,64 @@ class StatisticsClass:
         self.cases_score_statistics()
         self.action_statistics()
 
-'''
-TODO
-面向用例
-表头:user_id case_id 次数
-'''
+    def user_to_case_statistics(self):
+        '''
+        统计有效情况，不计入多线程计算部分
+        :return:
+        '''
+        # userid -> caseid -> [allUploads, validUploads]
+        list_result = []
+        for x in range(1, 20):
+            # 统计一个模块的部分
+            result = {}
+            json_path = "../JSON_Chunk/ChunkJSON/test_data_" + str(x) + ".json"
+            # 统计总共提交次数
+            with open(json_path, 'r') as f:
+                text = f.read()
+                data = json.loads(text)
+                user_ids = list(data.keys())
+                for user_id in user_ids:
+                    result[user_id] = {}
+                    cases = data[user_id]['cases']
+                    for case in cases:
+                        result[user_id][case['case_id']] = [len(case['upload_records'])]
+            # 统计有效次数
+            detail_path = "../OurModelOutPut/python_source/test_data_" + str(x) + "/CsvResult/detail.csv"
+            with open(detail_path, 'r') as f2:
+                data2 = csv.reader(f2)
+                for line in data2:
+                    if(line[0] == 'user_id'):
+                        continue
+                    if(len(result[line[0]][line[1]]) == 1):
+                        result[line[0]][line[1]].append(1)
+                    elif(len(result[line[0]][line[1]]) == 2):
+                        result[line[0]][line[1]][1] += 1
+                    else:
+                        print("ERROR!")
+            for user in result:
+                for case in result[user]:
+                    if(len(result[user][case]) == 1):
+                        result[user][case].append(0)
+                    valid = result[user][case][1]
+                    all = result[user][case][0]
+                    if(all == 0):
+                        valid_rate = 0
+                    else:
+                        valid_rate = valid / all
+                    list_result.append([user, case, all, valid, valid_rate])
+        # 输出到csv文件
+        output_path = "../OurModelOutPut/Valid"
+        if(not os.path.exists(output_path)):
+            os.makedirs(output_path)
+        list_result.insert(0,['user_id','case_id','all_uploads_number','valid_uploads_number','valid_rate'])
+        with open(output_path + "/" + "valid.csv", 'w', newline="") as f:
+            writer = csv.writer(f)
+            for line in list_result:
+                writer.writerow(line)
 
 if __name__ == '__main__':
     source = "./CsvResult/detail.csv"
     output = "./Statistic/"
     temp = StatisticsClass("temp", source, output)
-    temp.my_statistics()
+    # temp.my_statistics()
+    temp.user_to_case_statistics()
