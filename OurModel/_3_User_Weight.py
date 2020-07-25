23656dad509688f14a9a9a9a8e22ee6447d8e97f
stormbroken <181250184@smail.nju.edu.cn>
2020-07-25 17:31:50 +0800

    feat: rm idea of homework

--- OurModel/_3_User_Weight.py
+++ OurModel/_3_User_Weight.py
@@ -4,18 +4,16 @@ import csv
 from sklearn.decomposition import PCA
 
 '''
-计算用户得分权重
+计算用户得分权重,不要用非静态全局变量2333
 '''
 
 
 class GetWeightClass:
-
-    userid = []
-    caseid = []
-
     def __init__(self, source, output):
         self.source = source
         self.output = output
+        self.user_ids = []
+        self.case_ids = []
     '''
     读取csv文件
     :param
@@ -26,44 +24,41 @@ class GetWeightClass:
         lines = []
         with open(self.source, 'r', encoding='utf-8') as f:
             reader = csv.reader(f)
+            next(reader)
             for item in reader:
                 lines.append(item)
-        n = 1
         for item in lines:
             tmp = []
-            if n == 1:
-                n = 0
-                continue
+            self.user_ids.append(int(item[0]))  # user_ids
+            self.case_ids.append(int(item[1]))  # case_ids
+            tmp.append(100 / float(item[2]))  # 提交次数的负相关
+            tmp.append(float(item[5]))  # 最终得分
+            arr1 = item[3].split('|')  # 每次提交的得分
+            arr2 = item[4].split('|')  # 分数变化
+            tmp.append(float(arr1[0]))  # 第一次得分
+
+            tmp1 = []  # 记录所有的分数变化(>0)
+            if float(arr1[0]) > 0:
+                tmp1.append(float(arr1[0]))
+
+            for item in arr2:
+                if item == '':
+                    break
+                if float(item) > 0:
+                    tmp1.append(float(item))
+
+            sum = 0
+            for i in tmp1:
+                sum += i
+
+            if len(tmp1) == 0:
+                average_change = 0
             else:
-                self.userid.append(int(item[0]))  # userid
-                self.caseid.append(int(item[1]))  # caseid
-                tmp.append(100 / float(item[2]))  # 提交次数的负相关
-                tmp.append(float(item[5]))  # 最终得分
-                arr1 = item[3].split('|')  # 每次提交的得分
-                arr2 = item[4].split('|')  # 分数变化
-                tmp.append(float(arr1[0]))  # 第一次得分
+                average_change = sum / len(tmp1)
+            tmp.append(average_change)
 
-                tmp1 = []  # 记录所有的分数变化(>0)
-                if float(arr1[0]) > 0:
-                    tmp1.append(float(arr1[0]))
+            result.append(tmp)
 
-                for item in arr2:
-                    if item == '':
-                        break
-                    if float(item) > 0:
-                        tmp1.append(float(item))
-
-                sum = 0
-                for i in tmp1:
-                    sum += i
-
-                if len(tmp1) == 0:
-                    average_change = 0
-                else:
-                    average_change = sum / len(tmp1)
-                tmp.append(average_change)
-
-                result.append(tmp)
         lines.clear()
         a = np.array(result)
         return a
@@ -82,22 +77,18 @@ class GetWeightClass:
         return b
 
     def get_result(self):
-        result1 = []
+        result = []
         tmp = self.pca_method()
-        tmp1 = []
-        tmp1.append("user_id")
-        tmp1.append("case_id")
-        tmp1.append("result")
-        result1.append(tmp1)
-
-        for i in range(len(self.userid)):
+        title = ["user_id", "case_id", "result"]
+        result.append(title)
+        for i in range(len(self.user_ids)):
             tmp2 = []
-            tmp2.append(self.userid[i])
-            tmp2.append(self.caseid[i])
+            tmp2.append(self.user_ids[i])
+            tmp2.append(self.case_ids[i])
             tmp2.append(float(tmp[i]))
-            result1.append(tmp2)
+            result.append(tmp2)
 
-        return result1
+        return result
 
     def get_weight(self):
         print("Start get Weight!")
@@ -107,7 +98,6 @@ class GetWeightClass:
             for line in result:
                 writer.writerow(line)
         print("Finish get Weight!")
-
 
 if __name__ == '__main__':
     pca = GetWeightClass("../OurModelOutPut/Cases/action_statistics.csv",
